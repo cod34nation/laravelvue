5e8b1562b25e2277fcb89064aa4be4557a2a42fe
afrizal <afrizalwibisono681@gmail.com>
2019-02-11 23:51:43 +0700

    create user and progress bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,40 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import VueRouter from 'vue-router';
+import moment from 'moment';
+import { Form, HasError, AlertError } from 'vform'
+
+window.Form = Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+Vue.use(VueRouter)
+
+import VueProgressBar from 'vue-progressbar'
+
+Vue.use(VueProgressBar, {
+  color: 'rgb(143, 255, 199)',
+  failedColor: 'red',
+  height: '3px'
+})
+
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default},
+    { path: '/profile', component: require('./components/Profile.vue').default },
+    { path:'/users',component: require('./components/Users.vue').default}
+  ]
+
+  const router = new VueRouter({
+    mode:'history',  
+    routes
+  })
+  Vue.filter('upText',function(text){
+      return text.charAt(0).toUpperCase() + text.slice(1)
+  });
+
+  Vue.filter('myDate',function(created){
+      return moment(created).format('MMMM Do YYYY');
+  });
 
 /**
  * The following block of code may be used to automatically register your
@@ -29,5 +63,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    
+    router
 });
